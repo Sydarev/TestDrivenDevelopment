996497ef47a5f08fc5be59faa33d15c3c9c47856
Sudarev <soul7341@gmail.com>
2023-11-27 21:16:07 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,17 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import ru.netology.PhoneBook;
+
 public class PhoneBookTest {
+    String name1 = "Ilya";
+    String name2 = "Ivan";
+    int number1 = 8903;
+    int number2 = 9348;
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        Assertions.assertEquals(phoneBook.add(name1, number1), 1);
+        Assertions.assertEquals(phoneBook.add(name2, number2), 2);
+    }
 }
